ab45647ea1159a56c8dc6f79fc68ca02cccf7daf
Nehal Kamal <nehal.kamal213@gmail.com>
2025-04-09 07:14:05 +0200

    fix build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        "globDirectory": "dist",
-        "globPatterns": ["**/*.{js,css,html,ico,png,svg}"],
-        "globIgnores": [
-          "node_modules/**/*",
-          "sw.js",
-          "workbox-*.js",
-          "sw.js",
-          "workbox-*.js"
-        ]
-      },
-    }),
+    react(), svgr(),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   workbox: {
+    //     "globDirectory": "dist",
+    //     "globPatterns": ["**/*.{js,css,html,ico,png,svg}"],
+    //     "globIgnores": [
+    //       "node_modules/**/*",
+    //       "sw.js",
+    //       "workbox-*.js",
+    //       "sw.js",
+    //       "workbox-*.js"
+    //     ]
+    //   },
+    // }),
   ],
   build: {
     minify: 'esbuild',
